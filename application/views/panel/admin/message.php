2b275e876ab30b80a151a19487b211897510ca37
kguza <31211935+Wullop@users.noreply.github.com>
2017-11-01 01:19:20 +0700

    Update message.php

--- application/views/panel/admin/message.php
+++ application/views/panel/admin/message.php
@@ -2,7 +2,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">ข้อความแจ้งเตือน
+            <h1 class="page-header">จัดการเครดิทลูกค้า
             </h1>
         </div>
     </div>
